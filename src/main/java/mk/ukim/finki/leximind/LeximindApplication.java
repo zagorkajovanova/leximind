e8ac3e13321040a651ac63cc792cdfd25aa00ff1
Zagorka <jovanovazagorka@outlook.com>
2023-07-31 12:29:12 +0200

    Initial commit

---
+++ src/main/java/mk/ukim/finki/leximind/LeximindApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.leximind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeximindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeximindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/leximind/LeximindApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.leximind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeximindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
